2986a11e71029a5ebba6460624a98ee893f8ba75
YoussefOuirini <youssefouirini@gmail.com>
2018-02-17 17:51:25 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+// Seting up the libraries:
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const session= require('express-session');
+
+// Setting up the link to the database.
+const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+	host: 'localhost',
+	dialect: 'postgres',
+	define: {
+		timestamps: true
+	}
+})
+
+app.use('/', bodyParser());
+
+app.set('views', './');
+app.set('view engine', 'pug');
+app.use(express.static("public"));
+
+// Creates session when user logs in
+app.use(session({
+	secret: `${process.env.SECRET_SESSION}`,
+	resave: true,
+	saveUninitialized: false
+}));
+
+// Goes to the index page, which is the homepage
+app.get('/',  (req,res)=>{
+	res.render('public/views/index', {
+		// You can also use req.session.message so message won't show in the browser
+		messsage: req.session.message,
+		// message: req.query.message,
+		user: req.session.user
+	});
+});
+
+var server = app.listen(3000, function() {
+  console.log('The server is running at http//:localhost:' + server.address().port)
+});
