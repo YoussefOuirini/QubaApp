152ce1c15d01a50d475ba58037949fc260d13bf7
Youssef Ouirini <youssef.ouirini-cic.netherlands@ibm.com>
2019-09-02 16:57:38 +0200

    adding mongodb

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+var mongo = require('mongodb');
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  console.log("Database created!");
+  db.close();
+});
+
 app.use('/', bodyParser());
 
 var server = app.listen(3000, function() {
